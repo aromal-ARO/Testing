643b8312b6bb94f1b46792765db71c6398f961fb
Aromal Udayan <aromalu2021@gmail.com>
2024-12-25 20:15:27 +0530

    Initail commit

---
+++ sample.js
@@ -0,0 +1,12 @@
+let a = 0
+for (let i =0;i<=10;i++){
+     a+=10;
+}
+console.log(a)
+
+let sum = [2,3,4,5,6]
+sum.pop(0)
+console.log(sum)
+for(let i =0;i<sum.length;i++){
+    console.log(sum)
+}
